b9fca584689da66bbfc4dd26ace239f69a10eb30
goptun <ramos.matheus@icloud.com>
2024-05-21 10:16:17 -0300

    Implement calculation of energy from mass using Einstein's formula

---
+++ problem_set_0/einstein/einstein.py
@@ -0,0 +1,12 @@
+def main():
+    mass = int(input("Please, enter the mass in kilograms: "))
+
+    c = 300000000
+
+    energy = mass * c**2
+
+    print(energy)
+
+
+if __name__ == "__main__":
+    main()
